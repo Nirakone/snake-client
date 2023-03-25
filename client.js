2316f1c9ef6ed7ac17d38af2af51b5a61dbe55ed
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-03-25 01:31:46 -0400

    Initial file upload

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: // IP address here,
+    port: // PORT number here,
+  });
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log('Success! Connected!');
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = {connect};  
