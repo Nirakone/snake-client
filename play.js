bf2877d80ac5223f2f54b032ddd17ed22a702197
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-03-25 22:50:37 -0400

    updated with new requires

--- play.js
+++ play.js
@@ -1,23 +1,9 @@
 const net = require("net");
 const { connect } = require("./client");
-
-/*
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541, // PORT number here,
-  });
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log('Success! Connected!');
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-*/
 connect();
+
+setupInput();
+
